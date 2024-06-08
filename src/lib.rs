9195cf4a273f74873b449c88796cac0cf4275964
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-06-08 20:11:45 +0200

    Prime numbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {n}th prime number?")
+    // Helper function to check if a number is prime
+    fn is_prime(num: u32) -> bool {
+        num > 1 && (num == 2 || (num % 2 != 0 && !(3..=((num as f64).sqrt() as u32)).step_by(2).any(|i| num % i == 0)))
+    }
+
+    // Finding the nth prime
+    (1..).filter(|&x| is_prime(x)).nth(n as usize).unwrap()
 }
